6ed3ddb23bf500e2ee2f5a20fb028b90eb1ea3bd
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2020-12-23 08:52:48 +0900

    収支追加のページ（何も書いてない）

---
+++ lib/add_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Add extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("追加ページ"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: double.infinity,
+            
+          ),
+        ],
+      ),
+    );
+  }
+}
