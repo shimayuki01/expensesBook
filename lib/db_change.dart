7d898c5636b848019483812e33c32b9a054e89e5
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2021-01-13 01:10:02 +0900

    状態変化を通知するproviderをインポート

---
+++ lib/db_change.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart';
+import 'package:expenses_book_app/db_interface.dart';
+
+class DbSearch extends ChangeNotifier{
+  List<Expense> _monthexpense = <Expense>[];
+
+}
