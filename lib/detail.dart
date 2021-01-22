91a8dc7edb77708d5f90e5084278b811094bec62
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2021-01-22 14:20:39 +0900

    支出を表示する際にーがつかないようにした

--- lib/detail.dart
+++ lib/detail.dart
@@ -10,13 +10,13 @@ class DetailPage extends StatefulWidget {
 
   DetailPage({this.year, this.month});
 
-
   @override
   Detail createState() => new Detail();
 }
 
 class Detail extends State<DetailPage> {
   List<Expense> items;
+  int _money;
 
   Future<void> _getMap() async {
     await context.read(listProvider).getList(widget.year, widget.month);
@@ -31,50 +31,49 @@ class Detail extends State<DetailPage> {
       body: Container(
         height: double.infinity,
         child: FutureBuilder(
-          future: _getMap(),
-          builder: (context, snap) {
-            return Consumer(
-              builder: (context, watch, child) {
-                items = watch(listProvider).listExpense;
-                if (items != null) {
-                  return ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        //収支のリスト表示
-                        return ListTile(
-                          leading: Column(
-                            children: [
-                              items[index].money > 0 ? Text("収入") : Text("支出"),
-                              Text(items[index].year.toString() +
-                                  "/" +
-                                  items[index].month.toString() +
-                                  "/" +
-                                  items[index].day.toString()),
-                            ],
-                          ),
-                          title: Column(
-                            children: [
-                              Text(items[index].name),
-                            ],
-                          ),
-                          trailing: Text(items[index].money.toString()),
-                          onTap: () =>
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          DelUpdPage(data: items[index]))),
-                        );
-                      });
-                } else {
-                  return Center(
-                    child: Text("表示するものがありません"),
-                  );
-                }
-              },
-            );
-          }
-        ),
+            future: _getMap(),
+            builder: (context, snap) {
+              return Consumer(
+                builder: (context, watch, child) {
+                  items = watch(listProvider).listExpense;
+                  if (items != null) {
+                    return ListView.builder(
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          items[index].money > 0
+                              ? _money = items[index].money
+                              : _money = -items[index].money;
+                          //収支のリスト表示
+                          return ListTile(
+                            leading: Column(
+                              children: [
+                                items[index].money > 0
+                                    ? Text("収入")
+                                    : Text("支出"),
+                                Text(items[index].day.toString()),
+                              ],
+                            ),
+                            title: Column(
+                              children: [
+                                Text(items[index].name),
+                              ],
+                            ),
+                            trailing: Text(_money.toString()),
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        DelUpdPage(data: items[index]))),
+                          );
+                        });
+                  } else {
+                    return Center(
+                      child: Text("表示するものがありません"),
+                    );
+                  }
+                },
+              );
+            }),
       ),
     );
   }
