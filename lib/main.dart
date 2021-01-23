91965de00aba3a7e272bca82795b7901fc29ad98
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2021-01-24 01:35:49 +0900

    画面遷移をタブバーを用いて遷移するようにした

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:expenses_book_app/add_page.dart';
 import 'package:expenses_book_app/db_interface.dart';
 import 'package:expenses_book_app/month_sum_list.dart';
 import 'package:expenses_book_app/provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/all.dart';
@@ -28,10 +29,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: <TargetPlatform, PageTransitionsBuilder>{
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(), // iOS風
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.macOS: CupertinoPageTransitionsBuilder(),
+          },
+        ),
       ),
       home: MyHomePage(title: '家計簿アプリ'),
       localizationsDelegates: [
@@ -56,9 +61,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Expense test1 =
-      Expense(id: 1, year: 2021, month: 1, day: 7, name: "fafdaf", money: -300);
-
   MonthData _monthData;
   int _year = DateTime.now().year;
   int _month = DateTime.now().month;
@@ -76,79 +78,81 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: FutureBuilder(
-          future: _init(),
-          builder: (context, ddd) {
-            return FutureBuilder(
-                future: _getData(),
-                builder: (context, snapshot) {
-                  if (snapshot.data != null) {
-                    _monthData = snapshot.data;
-                    return Consumer(builder: (context, watch, child) {
-                      if (watch(thisMonthProvider).monthData != null)
-                        _monthData = watch(thisMonthProvider).monthData;
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 150),
-                        child: Container(
-                          child: ListView(
-                            children: <Widget>[
-                              ListTile(
-                                title: Text('今月の支出'),
-                                trailing: Text(_monthData.outgo.toString()),
-                              ),
-                              ListTile(
-                                title: Text('今月の収入'),
-                                trailing: Text(_monthData.income.toString()),
-                              ),
-                              ListTile(
-                                title: Text('計'),
-                                trailing: Text(_monthData.sum.toString()),
-                              ),
-                              ListTile(
-                                title: Container(
-                                  width: 50,
-                                  child: RaisedButton(
-                                    child: const Text('詳細'),
-                                    color: Colors.blue,
-                                    shape: const StadiumBorder(),
-                                    onPressed: () async {
-                                      //画面遷移（詳細のペ－ジ）
-                                      await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => DetailPage(
-                                                year: _year, month: _month)),
-                                      );
-                                    },
+      bottomNavigationBar: CupertinoTabScaffold(
+          tabBar: CupertinoTabBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.trending_up), title: Text('今月')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.format_list_bulleted), title: Text('過去の履歴')),
+            ],
+          ),
+          tabBuilder: (context, index) {
+            if (index == 0) {
+              return FutureBuilder(
+                  future: _init(),
+                  builder: (context, ddd) {
+                    return FutureBuilder(
+                        future: _getData(),
+                        builder: (context, snapshot) {
+                          if (snapshot.data != null) {
+                            _monthData = snapshot.data;
+                            return Consumer(builder: (context, watch, child) {
+                              if (watch(thisMonthProvider).monthData != null)
+                                _monthData = watch(thisMonthProvider).monthData;
+                              return Padding(
+                                padding: const EdgeInsets.only(top: 150),
+                                child: Container(
+                                  child: ListView(
+                                    children: <Widget>[
+                                      ListTile(
+                                        title: Text('今月の支出'),
+                                        trailing:
+                                            Text(_monthData.outgo.toString()),
+                                      ),
+                                      ListTile(
+                                        title: Text('今月の収入'),
+                                        trailing:
+                                            Text(_monthData.income.toString()),
+                                      ),
+                                      ListTile(
+                                        title: Text('計'),
+                                        trailing:
+                                            Text(_monthData.sum.toString()),
+                                      ),
+                                      ListTile(
+                                        title: Container(
+                                          width: 50,
+                                          child: RaisedButton(
+                                            child: const Text('詳細'),
+                                            color: Colors.blue,
+                                            shape: const StadiumBorder(),
+                                            onPressed: () async {
+                                              //画面遷移（詳細のペ－ジ）
+                                              await Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        DetailPage(
+                                                            year: _year,
+                                                            month: _month)),
+                                              );
+                                            },
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              ),
-                              ListTile(
-                                title: RaisedButton(
-                                  child: Text('過去の履歴'),
-                                  color: Colors.cyanAccent,
-                                  shape: const StadiumBorder(),
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                PastListPage()));
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    });
-                  } else {
-                    return Text("era");
-                  }
-                });
+                              );
+                            });
+                          } else {
+                            return Text("era");
+                          }
+                        });
+                  });
+            } else
+              return PastListPage();
           }),
 
       floatingActionButton: FloatingActionButton(
