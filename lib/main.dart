4ecddbae160fe01b87c41e38084394e92a86ad19
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2021-01-27 03:27:02 +0900

    今月の収支合計を表示させるclassを新たに作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:expenses_book_app/add_page.dart';
-import 'package:expenses_book_app/db_interface.dart';
 import 'package:expenses_book_app/month_sum_list.dart';
 import 'package:expenses_book_app/provider.dart';
+import 'package:expenses_book_app/this_month_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/all.dart';
-import 'dart:async';
-import 'detail.dart';
 import 'package:flutter/widgets.dart';
 
 final listProvider = ChangeNotifierProvider(
@@ -61,20 +59,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  MonthData _monthData;
-  int _year = DateTime.now().year;
-  int _month = DateTime.now().month;
-
-  Future<void> _init() async {
-    await DbInterface().init();
-    await context.read(listProvider).getList(_year, _month);
-    await context.read(pastMonthProvider).getList();
-  }
-
-  Future<MonthData> _getData() async {
-    MonthData aa = await DbInterface().monthSum(_year, _month);
-    return aa;
-  }
 
   int _index = 0;
 
@@ -107,66 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: _index == 0
-          ? FutureBuilder(
-              future: _init(),
-              builder: (context, ddd) {
-                return FutureBuilder(
-                    future: _getData(),
-                    builder: (context, snapshot) {
-                      if (snapshot.data != null) {
-                        _monthData = snapshot.data;
-                        return Consumer(builder: (context, watch, child) {
-                          if (watch(thisMonthProvider).monthData != null)
-                            _monthData = watch(thisMonthProvider).monthData;
-                          return Padding(
-                            padding: const EdgeInsets.only(top: 150),
-                            child: Container(
-                              child: ListView(
-                                children: <Widget>[
-                                  ListTile(
-                                    title: Text('今月の支出'),
-                                    trailing: Text(_monthData.outgo.toString()),
-                                  ),
-                                  ListTile(
-                                    title: Text('今月の収入'),
-                                    trailing:
-                                        Text(_monthData.income.toString()),
-                                  ),
-                                  ListTile(
-                                    title: Text('計'),
-                                    trailing: Text(_monthData.sum.toString()),
-                                  ),
-                                  ListTile(
-                                    title: Container(
-                                      width: 50,
-                                      child: RaisedButton(
-                                        child: const Text('詳細'),
-                                        color: Colors.blue,
-                                        shape: const StadiumBorder(),
-                                        onPressed: () async {
-                                          //画面遷移（詳細のペ－ジ）
-                                          await Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    DetailPage(
-                                                        year: _year,
-                                                        month: _month)),
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
-                      } else {
-                        return Text("era");
-                      }
-                    });
-              })
+          ? ThisMonthPage()
           : PastListPage(),
     );
   }
---
+++ lib/this_month_data.dart
@@ -0,0 +1,99 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'db_interface.dart';
+import 'package:expenses_book_app/main.dart';
+
+import 'detail.dart';
+
+class ThisMonthPage extends StatefulWidget{
+  @override
+  ThisMonth createState() => new ThisMonth();
+}
+
+class ThisMonth extends State<ThisMonthPage>{
+  MonthData _monthData;
+  int _year = DateTime.now().year;
+  int _month = DateTime.now().month;
+
+  Future<void> _init() async {
+    await DbInterface().init();
+    await context.read(listProvider).getList(_year, _month);
+    await context.read(pastMonthProvider).getList();
+  }
+
+  Future<MonthData> _getData() async {
+    MonthData aa = await DbInterface().monthSum(_year, _month);
+    return aa;
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: CupertinoNavigationBar(
+          middle: Text("今月の収支"),
+        ),
+        body:FutureBuilder(
+            future: _init(),
+            builder: (context, ddd) {
+              return FutureBuilder(
+                  future: _getData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data != null) {
+                      _monthData = snapshot.data;
+                      return Consumer(builder: (context, watch, child) {
+                        if (watch(thisMonthProvider).monthData != null)
+                          _monthData = watch(thisMonthProvider).monthData;
+                        return Padding(
+                          padding: const EdgeInsets.only(top: 150),
+                          child: Container(
+                            child: ListView(
+                              children: <Widget>[
+                                ListTile(
+                                  title: Text('今月の支出'),
+                                  trailing: Text(_monthData.outgo.toString()),
+                                ),
+                                ListTile(
+                                  title: Text('今月の収入'),
+                                  trailing:
+                                  Text(_monthData.income.toString()),
+                                ),
+                                ListTile(
+                                  title: Text('計'),
+                                  trailing: Text(_monthData.sum.toString()),
+                                ),
+                                ListTile(
+                                  title: Container(
+                                    width: 50,
+                                    child: RaisedButton(
+                                      child: const Text('詳細'),
+                                      color: Colors.blue,
+                                      shape: const StadiumBorder(),
+                                      onPressed: () async {
+                                        //画面遷移（詳細のペ－ジ）
+                                        await Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  DetailPage(
+                                                      year: _year,
+                                                      month: _month)),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                    } else {
+                      return Text("error");
+                    }
+                  });
+            })
+    );
+  }
+
+}
