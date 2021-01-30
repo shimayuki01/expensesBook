81b3a21f36374e59ec852cfc91c063209bff7c2f
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2021-01-30 19:14:16 +0900

    detepickerに移行したときに"完了"と"キャンセル"を押せるようにした 日付選択の部分だけ白くした　広告調整

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -15,6 +15,7 @@ class AddPage extends StatefulWidget {
 class Add extends State<AddPage> {
   String _payment = 'out';
   DateTime _date = DateTime.now();
+  DateTime _selectDate;
   String _name = '';
   int _money = 0;
   int _id;
@@ -66,111 +67,150 @@ class Add extends State<AddPage> {
             future: _setMaxId(),
             builder: (context, snapshot) {
               //if (snapshot.connectionState == ConnectionState.done) {
-                _id = snapshot.data;
-                return SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      AdmobBanner(
-                        adUnitId: AdMobService().getBannerAdUnitId(),
-                        adSize: AdmobBannerSize(
-                          width: MediaQuery.of(context).size.width.toInt(),
-                          height: AdMobService().getHeight(context).toInt(),
-                          name: 'SMART_BANNER',
-                        ),
-                      ),
-                      AdmobBanner(
-                        adUnitId: AdMobService().getBannerAdUnitId(),
-                        adSize: AdmobBannerSize(
-                          width: MediaQuery.of(context).size.width.toInt(),
-                          height: AdMobService().getHeight(context).toInt(),
-                          name: 'SMART_BANNER',
-                        ),
-                      ),
-                      //収支のラジオボタン
-                      RadioListTile(
-                          title: Text('収入'),
-                          value: 'in',
-                          groupValue: _payment,
-                          onChanged: _onChanged),
-                      RadioListTile(
-                          title: Text('支出'),
-                          value: 'out',
-                          groupValue: _payment,
-                          onChanged: _onChanged),
+              _id = snapshot.data;
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    // AdmobBanner(
+                    //   adUnitId: AdMobService().getBannerAdUnitId(),
+                    //   adSize: AdmobBannerSize(
+                    //     width: MediaQuery.of(context).size.width.toInt(),
+                    //     height: AdMobService().getHeight(context).toInt(),
+                    //     name: 'SMART_BANNER',
+                    //   ),
+                    // ),
+                    //収支のラジオボタン
+                    RadioListTile(
+                        title: Text('収入'),
+                        value: 'in',
+                        groupValue: _payment,
+                        onChanged: _onChanged),
+                    RadioListTile(
+                        title: Text('支出'),
+                        value: 'out',
+                        groupValue: _payment,
+                        onChanged: _onChanged),
 
-                      //日付の入力
-                      FlatButton(
-                          child: Text(_date.year.toString() +
-                              '/' +
-                              _date.month.toString() +
-                              '/' +
-                              _date.day.toString()),
-                          textColor: Colors.black,
-                          onPressed: () {
-                            showCupertinoModalPopup(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return Container(
-                                    height: 150,
-                                    child: CupertinoDatePicker(
-                                      mode: CupertinoDatePickerMode.date,
-                                      initialDateTime: _date,
-                                      onDateTimeChanged: (DateTime selectDate) {
-                                        setState(() {
-                                          _date = selectDate;
-                                        });
-                                      },
+                    //日付の入力
+                    FlatButton(
+                        child: Text(_date.year.toString() +
+                            '/' +
+                            _date.month.toString() +
+                            '/' +
+                            _date.day.toString()),
+                        textColor: Colors.black,
+                        onPressed: () {
+                          showCupertinoModalPopup(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Color(0xffffffff),
+                                        border: Border(
+                                          bottom: BorderSide(
+                                            color: Color(0xff999999),
+                                            width: 0.0,
+                                          ),
+                                        ),
+                                      ),
+                                      child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            CupertinoButton(
+                                              child: Text('キャンセル'),
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                            CupertinoButton(
+                                              child: Text('完了'),
+                                              onPressed: () {
+                                                setState(() {
+                                                  _date = _selectDate;
+                                                });
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                          ]),
                                     ),
-                                  );
-                                });
-                          }),
+                                    Container(
+                                      height:
+                                          MediaQuery.of(context).size.height /
+                                              3,
+                                      decoration: BoxDecoration(
+                                        color: Color(0xffffffff),
+                                        border: Border(
+                                          bottom: BorderSide(
+                                            color: Color(0xff999999),
+                                            width: 0.0,
+                                          ),
+                                        ),
+                                      ),
+                                      child: CupertinoDatePicker(
+                                        mode: CupertinoDatePickerMode.date,
+                                        initialDateTime: _date,
+                                        onDateTimeChanged:
+                                            (DateTime newDateTime) {
+                                          setState(
+                                              () => _selectDate = newDateTime);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              });
+                        }),
 
-                      //名称の入力
-                      TextField(
-                        maxLength: 20,
-                        maxLengthEnforced: true,
-                        maxLines: 1,
-                        decoration: const InputDecoration(
-                            hintText: '入力してください', labelText: '名称'),
-                        onChanged: _handleName,
-                      ),
+                    //名称の入力
+                    TextField(
+                      maxLength: 20,
+                      maxLengthEnforced: true,
+                      maxLines: 1,
+                      decoration: const InputDecoration(
+                          hintText: '入力してください', labelText: '名称'),
+                      onChanged: _handleName,
+                    ),
 
-                      //金額の入力
-                      TextField(
-                        maxLength: 7,
-                        maxLengthEnforced: true,
-                        maxLines: 1,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
-                        ],
-                        decoration: const InputDecoration(
-                            hintText: '1000', labelText: '金額'),
-                        onChanged: _handleMoney,
-                      ),
-                      RaisedButton(
-                          child: const Text("追加"),
-                          color: Colors.blue,
-                          onPressed: () async {
-                            //追加処理
-                            if (_payment == 'out') {
-                              _money = -_money;
-                            }
-                            Expense add = Expense(
-                                id: _id,
-                                year: _date.year,
-                                month: _date.month,
-                                day: _date.day,
-                                name: _name,
-                                money: _money);
-                            await DbInterface().insertExpense(add);
-                            _handleId();
-                            await context.read(thisMonthProvider).getMonthData(
-                                DateTime.now().year, DateTime.now().month);
-                            Navigator.pop(context);
-                          }),
-                    ],
-                  ),
-                );
+                    //金額の入力
+                    TextField(
+                      maxLength: 7,
+                      maxLengthEnforced: true,
+                      maxLines: 1,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+                      ],
+                      decoration: const InputDecoration(
+                          hintText: '1000', labelText: '金額'),
+                      onChanged: _handleMoney,
+                    ),
+                    RaisedButton(
+                        child: const Text("追加"),
+                        color: Colors.blue,
+                        onPressed: () async {
+                          //追加処理
+                          if (_payment == 'out') {
+                            _money = -_money;
+                          }
+                          Expense add = Expense(
+                              id: _id,
+                              year: _date.year,
+                              month: _date.month,
+                              day: _date.day,
+                              name: _name,
+                              money: _money);
+                          await DbInterface().insertExpense(add);
+                          _handleId();
+                          await context.read(thisMonthProvider).getMonthData(
+                              DateTime.now().year, DateTime.now().month);
+                          Navigator.pop(context);
+                        }),
+                  ],
+                ),
+              );
               // } else {
               //   return Center(child: CupertinoActivityIndicator());
               // }
